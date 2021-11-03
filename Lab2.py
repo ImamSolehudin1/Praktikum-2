8d07687362ed62eb081fe9990344c35312cb66f1
Imam Solehudin <imamsolehudin59@gmail.com>
2021-11-03 20:58:15 +0700

    Menambahkan file python

---
+++ Lab2.py
@@ -0,0 +1,10 @@
+a=int(input("masukan nilai a:"))
+b=int(input("masukan nilai b:"))
+print("variable a=",a)
+print("variable b=",b)
+print("hasil penggabungan {0}&{1}=%d".format(a,b) %(a+b))
+
+a=int(a)
+b=int(b)
+print("hasil penjumlahan {0}+{1}=%d".format(a,b) %(a+b))
+print("hasil pembagian {0}/{1}=%d".format(a,b) %(a/b))
---
+++ Mencariluasdankelilinglingkaran.py
@@ -0,0 +1,13 @@
+print ("Menghitung Luas dan Keliling Lingkaran")
+print ("==========================================")
+
+r = int (input ("Masukan jari-jari lingkaran : "))
+
+phi = 3.14
+luas  = phi*r*r
+keliling = 2*phi*r
+
+print ("Luas lingkaran adalah          = ", luas)
+print ("dan")
+print ("Keliling Lingkaran adalah      =", keliling)
+print ("==========================================")
